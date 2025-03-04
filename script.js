86cdaaa96b65a367ee57bd855861a466ee7324a6
HARSHAVARDHAN <harshavardhanharsha37@gmail.com>
2025-03-04 22:38:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+    });
+});
